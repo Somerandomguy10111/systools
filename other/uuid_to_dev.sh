8a04017a93b79e519acad9500baf971a2c509071
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-12-08 22:35:36 +0100

    Introduced unique identifier to logical name mapping script

--- other/uuid_to_dev.sh
+++ other/uuid_to_dev.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Function to map UUID to its device path
 #map_uuid_to_dev() {
 #    for dev in $(blkid | cut -d: -f1); do
 #        uuid=$(blkid -s UUID -o value $dev)
@@ -12,24 +11,19 @@
 #    echo "No device found for UUID $1"
 #}
 
-# Reading UUID
 #read -p "Enter UUID: " uuid
 
-# Mapping
 #dev_path=$(map_uuid_to_dev $uuid)
 
-# Output
 #echo "Device Path: $dev_path"
 
-map_uuid_to_dev() {
-   for dev in $(blkid | cut -d: -f1); do
-	dev_uuid=$(blkid -o value -s UUID $dev)
-	if [ "$dev_uuid" = "$1" ]; then
-#	if [ true ]; then
-		echo $dev
-	fi
-   done
-}
+# To find the logical name from a known identifier
+identifier="nvme-nvme.c0a9-323332324536444430343632-435431303030503353534438-00000001"
+logical_name=$(readlink -f /dev/disk/by-id/$identifier)
+echo $logical_name
 
-echo $(map_uuid_to_dev $1)
+# To find the identifier from a known logical name
+#logical_name="/dev/sda"
+#identifier=$(ls -l /dev/disk/by-id/ | grep "$logical_name$" | awk '{print $9}')
+#echo "Identifier: $identifier"
 
