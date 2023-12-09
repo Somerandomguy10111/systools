d971d863493dfb3fc06586d293c3c05303ee4f8b
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-12-09 17:22:13 +0100

    Now wipes paritition table instead of disk

--- lvm.sh
+++ lvm.sh
@@ -1,4 +1,4 @@
-## Creates an LVM copy of a physical disk on a target disk
+## Creates an LVM copy of a "main disk" on a "target disk"
 
 # Input variables
 
@@ -22,34 +22,36 @@ target_disk=$(id_to_logical_name $target_unique_id)
 #---------------------#
 
 # Unmount the target disk if it is mounted
-echo "Unmounting target disk $target_disk"
+echo "-> Unmounting target disk $target_disk"
 sudo umount "/dev/$target_disk" 2> /dev/null || true
 
 #Wipe the target
-echo "Wiping target disk $target_disk"
-sudo bash -c "pv < /dev/zero > $target_disk"
-
+echo "-> Wiping partition table of target disk $target_disk"
+sudo dd if=/dev/zero of="$" bs=512 count=1
 
 # Make Volume group on target
-echo "Creating volume group $vg_name on target disk $target_disk"
+echo "-> Creating volume group $vg_name on target disk $target_disk"
 sudo pvcreate "$target_disk"
 sudo vgcreate $vg_name "$target_disk"
 
 
 # Create create LV copies of partition on target
 partitions=$(lsblk -lno NAME,TYPE "$main_disk" | grep 'part' | awk '{print $1}')
-echo "$partitions"
+
 for partition in $partitions; do
     # Get the size of the partition
     size=$(sudo blockdev --getsize64 /dev/"$partition")
-    echo "Recognized partition $partition of size $size bytes"
+    echo " "
+    echo "-> Recognized partition $partition of size $size bytes on $main_disk"
 
     # Create a corresponding logical volume of the same size
     lv_name="${lv_name_prefix}_$(basename "$partition")"
-    echo "Creating LVM copy of parition with name $lv_name"
+    echo "-- Creating LMV partition mirroring $partition with name $lv_name on $vg_name"
     sudo lvcreate -L "${size}"b -n "$lv_name" "$vg_name"
 
     # Clone the partition to the new logical volume
-    echo "Cloning partition $partition to logical volume $lv_name"
+    echo "-- Cloning partition $partition to logical volume $lv_name"
     sudo bash -c "pv < /dev/$partition > /dev/$vg_name/$lv_name"
 done
+
+echo 'done'
