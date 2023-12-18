e72f32eba865dce851546d72b44ecb5c484c24c0
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-12-18 16:29:02 +0100

    updated make lvm

--- partition_operations/lvm_copy.sh
+++ partition_operations/lvm_copy.sh
@@ -3,7 +3,7 @@
 ## Creates an LVM copy of a "main disk" on a "target disk"
 
 # Input variables for boot and filesystem partitions
-target_disk='/dev/sdX'
+target_disk='/dev/nvme0n1'
 boot_partition="/dev/sda1"
 fsys_partition="/dev/sda2"
 
@@ -12,24 +12,26 @@ lv_name_prefix="lv"
 
 #---------------------#
 boot_size=$(sudo blockdev --getsize64 "$boot_partition")
-new_boot_dev_name="${target_disk}1"
+new_boot_dev_name="${target_disk}p1"
 
 echo "-> Cloning boot $boot_partition to $new_boot_dev_name of size $boot_size bytes"
-#sudo parted "$target_disk" mkpart primary fat32 1MiB $((boot_size / 1024 / 1024 + 1))MiB
-#sudo bash -c "pv < $boot_partition > $new_boot_dev_name"
 
-new_lvm_part_name="${target_disk}2"
+sudo parted /dev/nvme0n1 mklabel gpt
+sudo parted "$target_disk" mkpart primary fat32 1MiB $((boot_size / 1024 / 1024 + 1))
+sudo bash -c "pv < $boot_partition > $new_boot_dev_name"
+
+new_lvm_part_name="${target_disk}p2"
 echo "-> Creating new partition for LVM on $new_lvm_part_name"
-#sudo parted "$target_disk" mkpart primary ext4 $((boot_size / 1024 / 1024 + 1))MiB 100%
+sudo parted "$target_disk" mkpart primary ext4 $((boot_size / 1024 / 1024 + 1))MiB 100%
 
 echo "-> Creating Physical Volume on $new_lvm_part_name"
-#sudo pvcreate "$new_lvm_part_name"
+sudo pvcreate "$new_lvm_part_name"
 echo "-> Creating Volume Group $vg_name"
-#sudo vgcreate "$vg_name" "$new_lvm_part_name"
+sudo vgcreate "$vg_name" "$new_lvm_part_name"
 
 new_lv_dev_name="/dev/$vg_name/${lv_name_prefix}_$(basename "$fsys_partition")"
 fsys_size=$(sudo blockdev --getsize64 "$fsys_partition")
 
 echo "-> Creating LVM for filesystem partition $fsys_partition on $new_lv_dev_name of size $fsys_size bytes"
-#sudo lvcreate -L "${fsys_size}"b -n "${lv_name_prefix}_$(basename "$fsys_partition")" "$vg_name"
-#sudo bash -c "pv < $fsys_partition > $new_lv_dev_name"
+sudo lvcreate -L "${fsys_size}"b -n "${lv_name_prefix}_$(basename "$fsys_partition")" "$vg_name"
+sudo bash -c "pv < $fsys_partition > $new_lv_dev_name"
