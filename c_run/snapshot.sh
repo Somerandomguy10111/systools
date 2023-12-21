bdcef60ee05ef9b71f6a6f59de46f64742fe898e
Daniel Hollarek <daniel.hollarek@googlemail.com>
2023-12-21 15:49:59 +0100

    Eliminated unnecessary functionalities

--- c_run/snapshot.sh
+++ c_run/snapshot.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-source lib/name_resolution.sh
-
-# Input parameters
-part_id="dm-name-VG-lv_nvme0n1p2"
+source devices.sh
 
 #---------------------#
 #!/bin/bash
-
-# Function to get the full logical volume path
-get_full_lv_path() {
-    local dev=$1
-    local vg_name
-    local lv_name
-
-    vg_name=$(dmsetup info -c --noheadings -o vg_name "$dev")
-    lv_name=$(dmsetup info -c --noheadings -o lv_name "$dev")
-    echo "/dev/$vg_name/$lv_name"
-}
 
 # Function to create a snapshot of the logical volume
 create_snapshot() {
@@ -38,11 +24,5 @@ create_snapshot() {
 }
 
 
-# Main script logic
-part_id="dm-name-VG-lv_nvme0n1p2"
-fsys_dev=$(id_to_logical_name "$part_id")
-full_lv_path=$(get_full_lv_path "$fsys_dev")
 
-echo "-> Found logical volume: $full_lv_path"
-
-create_snapshot "$full_lv_path"
+create_snapshot "$fsys1"
